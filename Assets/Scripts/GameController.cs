3cb5385f8a710df80e21620d02fa8143fb6ca604
AustrianGameDev <austriangamedev@gmail.com>
2022-07-10 22:24:57 +0200

    Bug fix in GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
@@ -120,7 +121,7 @@ public class GameController : MonoBehaviour
     
     private BigInteger perSecondAmount()
     {
-        switch(clickLvl)
+        switch(perSecondLvl)
         {
             case 1: return 1;
             case 2: return 3;
