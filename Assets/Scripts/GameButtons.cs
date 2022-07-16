fd2751e5f5eed1e729d7a518c1ee875d5e671b8f
AustrianGameDev <austriangamedev@gmail.com>
2022-07-16 15:28:45 +0200

    Added click sound for game

--- Assets/Scripts/GameButtons.cs
+++ Assets/Scripts/GameButtons.cs
@@ -19,13 +19,19 @@ public class GameButtons : MonoBehaviour
     [SerializeField] private Button bt_saveAndQuit;
 
     [SerializeField] private SceneAsset mainScene;
+    
+    [SerializeField] private AudioClip click;
 
     private GameController gameController;
+    private float volume = 1f;
     
     // Start is called before the first frame update
     void Start()
     {
         gameController = GetComponent<GameController>();
+        
+        Settings settings = SaveSystem.LoadSettings();
+        volume = settings.volume;
         
         bt_upgradeClick.onClick.AddListener(onUpgradeClick);
         bt_upgradePerSecond.onClick.AddListener(onUpgradePerSecond);
@@ -37,28 +43,45 @@ public class GameButtons : MonoBehaviour
     private void onUpgradeClick()
     {
         gameController.UpgradeClick();
+        playClick();
     }
 
     private void onUpgradePerSecond()
     {
         gameController.UpgradePerSecond();
+        playClick();
     }
 
     private void onShowStats()
     {
         notStatistics.SetActive(false);
         statistics.SetActive(true);
+        playClick();
     }
 
     private void onBackStats()
     {
         notStatistics.SetActive(true);
         statistics.SetActive(false);
+        playClick();
     }
 
     private void onSaveAndQuit()
     {
+        playClick();
+        StartCoroutine(doSaveAndQuit());
+    }
+
+    private IEnumerator doSaveAndQuit()
+    {
+        yield return new WaitForSeconds(0.2f);
+        
         SaveSystem.SaveGame(gameController);
         SceneManager.LoadScene(mainScene.name);
+    }
+    
+    private void playClick()
+    {
+        AudioSource.PlayClipAtPoint(click, transform.position, volume);
     }
 }
